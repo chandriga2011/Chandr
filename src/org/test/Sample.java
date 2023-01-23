907d3baf76394dd361648e9f43d713826f481069
chandriga2011 <chandrigads@gmail.com>
2023-01-23 23:53:35 +0530

    code modified

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -6,8 +6,11 @@ public static void main(String[] args) {
 	int a=20;
 	if (a>=10) {
 		System.out.println("a is greater than 10");
+//	}else {
+		//	System.out.println("a is lesser than 10");
 	}else {
-			System.out.println("a is lesser than 10");
+		System.out.println("a is lesser than 10");
+		
 		}
 	}
 }
