dd8f622ce5d74c50c8021fd02eb294668271c5af
chandriga2011 <chandrigads@gmail.com>
2023-01-23 21:14:56 +0530

    new code accepted

---
+++ src/org/test/Sample.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Sample {
+public static void main(String[] args) {
+	
+	int a=20;
+	if (a>=10) {
+		System.out.println("a is greater than 10");
+	}else {
+			System.out.println("a is lesser than 10");
+		}
+	}
+}
